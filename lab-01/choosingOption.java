891fc5661baf7b31d91b759f0addf7c3e08619d2
minhDTN <77446678+minhDTN@users.noreply.github.com>
2021-02-26 14:24:28 +0700

    ex6.1

--- lab-01/choosingOption.java
+++ lab-01/choosingOption.java
@@ -2,10 +2,11 @@
 Write, compile and run the ChoosingOption program:
     Note: We use JavaBasics project for this exercise
 */
+
 import javax.swing.JOptionPane;
 public class choosingOption {
     public static void main(String[] args) {
-		// TODO Auto-generated method stub
+// TODO Auto-generated method stub
 		int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
 		JOptionPane.showMessageDialog(null, "You've chosen: " + (option == JOptionPane.YES_OPTION?"YES":"NO"));
 		System.exit(0);
